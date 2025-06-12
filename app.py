e3aa6ab9c9d6407c8cb559476cc8976f8c735020
DaveSV <alb.saenz@gmail.com>
2025-06-11 21:32:35 -0600

    Actualiza app.py con el modelo Gemma

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('Gemma 3 12B')
 
 import os
 my_api_key_gemini = os.getenv('api_key_gemini')
