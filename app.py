fccad87c37c473aa6c37453a0b549ed5bab2d961
DaveSV <alb.saenz@gmail.com>
2025-06-11 23:26:50 -0600

    Actualizando de archivo principa app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-model = genai.GenerativeModel('gemma3_1b')
+model = genai.GenerativeModel('gemma-3b')
 
 import os
 my_api_key_gemini = os.getenv('api_key_gemini')
